944b4eafca026dd6b3d6891643b01fcde68e4a95
my-totoro <twang2015@outlook.com>
2018-07-15 22:54:06 +0800

    More control operations

--- src/scala/hk/edu/cuhk/cse/bigdata/scalacookbook/control/AdvancedControls.scala
+++ src/scala/hk/edu/cuhk/cse/bigdata/scalacookbook/control/AdvancedControls.scala
@@ -20,4 +20,28 @@ object AdvancedControls {
     println(factorial(10))
   }
 
+  // use if to implement ternary operators in Java
+  def ifAsTernaryOperator(): Unit = {
+    def abs(x: Int) = if(x >= 0) x else -x
+    def max(x: Int, y: Int) = if(x >= y) x else y
+  }
+
+  // match statement
+  case class Person(name: String)
+  def getClassAsString(x: Any): String = x match {
+    case s: String => s + "is a String"
+    case i: Int => "Int"
+    case f: Float => "Float"
+    case l: List[_] => "List"
+    case p: Person => "Person"
+    case _ => "Unknow" // case default => println(default)
+  }
+
+  // match multiple conditions with one case
+  def matchMultipleInOneCase(i: Int): Unit = i match {
+    case 1 | 3 | 5 | 7 | 9 => println("Odd")
+    case 2 | 4 | 6 | 8 | 10 => println("Even")
+    case default => println(default)
+  }
+
 }
